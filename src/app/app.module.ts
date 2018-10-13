2abca53ea8d9103f4b8471b7ecc17ba06dfa5128
Ekaterina Mozheiko <ek.mozheiko@gmail.com>
2018-10-13 22:39:45 +0700

    TASK-1 add header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {
+  MatButtonModule,
+  MatMenuModule,
+  MatIconModule
+} from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/header/header.component.module.ts
@@ -0,0 +1,7 @@
+import { MatButtonModule, MatMenuModule, MatIconModule } from '@angular/material';
+import { NgModule } from '@angular/core';
+
+@NgModule({
+  imports: [ MatButtonModule, MatMenuModule, MatIconModule ]
+})
+export class HeaderComponentModule { }
